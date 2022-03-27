90032595efcb775e25d27fd874beac59c602894e
Sirishakopparaju <sirishakopparaju98@gmail.com>
2022-03-27 22:46:10 +0530

    refactor uc1.1

--- src/SnakeAndLadder/SnakeAndLadder.java
+++ src/SnakeAndLadder/SnakeAndLadder.java
@@ -1,5 +1,10 @@
 package SnakeAndLadder;
 
 public class SnakeAndLadder {
+	public void main(String[]args) {
+		int Dice=(int) (Math.floor(Math.random()%6)+1);
+		System.out.println("the dice value"+Dice);
+		
+	}
 
 }
